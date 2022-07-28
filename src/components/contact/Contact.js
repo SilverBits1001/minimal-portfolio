30595b384c8bed5a8fdfeaeb0e6bad4cf20436db
Corey Coleman <coreycoleman1339@gmail.com>
2022-07-28 14:25:36 -0700

    contact started2

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -15,7 +15,7 @@ export default function Contact() {
                             Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!
                         </p>
                         <div className='center'>
-                            <a className='button blue contact-button'>Say Hello</a>
+                            <a href='#projects' className='button blue contact-button'>Say Hello</a>
                         </div>
                     </Col>
                 </Row>
