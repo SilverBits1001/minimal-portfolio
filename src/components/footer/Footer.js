82a8ec8e6c01f505487d02b98c9eb604fb52ab28
Corey Coleman <coreycoleman1339@gmail.com>
2023-01-19 15:36:31 -0800

    footer links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import {  FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export default function Footer() {
     return (
-        <div className='footer-wrapper d-flex align-items-center justify-content-center'> 
-       
+        <div className='footer-wrapper d-flex align-items-center justify-content-center'>
+
             <div className=''>
-               GitHi <FaGithub /> | <FaLinkedin />
+            <a target="_blank" rel="noreferrer" href="http://www.github.com/silverbits1001">
+                    <FaGithub />
+                </a>   |
+                <a target="_blank" rel="noreferrer" href="http://www.linkedin.com/in/corey-coleman-881983218">
+                    <FaLinkedin />
+                </a>
             </div>
         </div>
     )
 }
+
+
